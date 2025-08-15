c36f28f90c29af59dd476fa23ac9673853cd8fd2
KoushikSagarr <shadowslayerryt@gmail.com>
2025-08-15 11:48:21 +0530

    vercel patch

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // This tells Vite to use relative paths for assets in the build
+  plugins: [react({
+    jsxRuntime: 'automatic'
+  })],
   base: './',
 })
