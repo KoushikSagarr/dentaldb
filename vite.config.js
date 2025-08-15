7072ea82a5b083e86021fe19a8f9bbe0723ec7a6
KoushikSagarr <shadowslayerryt@gmail.com>
2025-08-15 12:01:04 +0530

    vercel patch

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxRuntime: 'automatic'
-  })],
-  base: './',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  base: './'
 })
